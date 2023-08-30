11b4799ea7334d7ba629ddc6395c28fafa74cedd
Chris Windsor <ccw3102@icloud.com>
2023-08-29 23:07:46 -0600

    Removed hardcoded public key from mapbox demo plugin

--- src/lib/plugins/checkout/storeSearch.svelte
+++ src/lib/plugins/checkout/storeSearch.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import { cart } from '$lib/stores/cart';
 	import { Map, Marker, controls } from '@beyonk/svelte-mapbox';
-
+	import { PUBLIC_MAPBOX_KEY } from '$env/static/public';
 	const { GeolocateControl, NavigationControl, ScaleControl } = controls;
 
 	let mapComponent: Map;
 
-	function handleReady() {
+	function handleMapReady() {
 		mapComponent.setCenter([-111.8508, 40.3916], 20);
 		mapComponent.flyTo({ center: [-111.8508, 40.3916] });
 	}
 </script>
 
-{#if $cart.items.filter( (item) => item.attributes.find((attribute) => attribute.label === 'Has Lemonade') ).length}
+{#if $cart.items.filter((item) => item.attributes.find((attribute) => attribute.id === 4)).length}
 	<div class="mt-10 border-t border-gray-200 pt-10">
 		<h2 class="text-lg font-medium text-gray-900">Select a store for pickup</h2>
 		<div class="mt-4">
@@ -25,16 +25,16 @@
 					class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 					name="store-search"
 					type="text"
-					placeholder="My hometown lemonade stand"
+					placeholder="Search by postal code or city & state"
 				/>
 			</div>
 		</div>
 		<div class="h-60 mt-6 rounded overflow-hidden">
 			<Map
 				bind:this={mapComponent}
-				accessToken={'pk.eyJ1IjoiY2hyaXN3aW5kc29yZGV2IiwiYSI6ImNsaXBhZ2lnZjA0YjYzY3F2bGdscTV5dTEifQ.0Tff8XcBuYxvi0SSn6jPsQ'}
+				accessToken={PUBLIC_MAPBOX_KEY}
 				options={{ scrollZoom: false }}
-				on:ready={handleReady}
+				on:ready={handleMapReady}
 			>
 				<Marker lat={40.3916} lng={-111.8508} color={123123} label="The Lemonade Stand" />
 				<NavigationControl />
